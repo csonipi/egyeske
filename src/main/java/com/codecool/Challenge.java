07a050280e2c58fbf26a5eac82db795f951f1cde
csonipi <csoni.pi@gmail.com>
2021-03-26 11:00:48 +0100

    All green

--- src/main/java/com/codecool/Challenge.java
+++ src/main/java/com/codecool/Challenge.java
@@ -3,6 +3,19 @@ package com.codecool;
 public class Challenge {
 
     public static int minPositive(int a, int b) {
+        boolean aPos = a>0;
+        boolean bPos = b>0;
+
+        if(aPos){
+            if(bPos){
+                return a>b ? b : a;
+            }
+            return a;
+        }
+        if(bPos){
+            return b;
+        }
+        return -1;
 
     }
 }
